c556d5ce3237925b7ba44eb43f6aede3895b6cf6
Santiago Caci <santiago.caci@gmail.com>
2022-08-15 15:50:10 -0300

    Post Entry added

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '../../../database';
 import { Entry, IEntry } from '../../../models';
 
-type Data = { message: string } | IEntry[];
+type Data = { message: string } | IEntry[] | IEntry;
 
 export default function handlerEntries(
   req: NextApiRequest,
@@ -11,7 +11,8 @@ export default function handlerEntries(
   switch (req.method) {
     case 'GET':
       return getEntries(res);
-
+    case 'POST':
+      return createEntry(req, res);
     default:
       return res.status(400).json({ message: 'Invalid endpoint' });
   }
@@ -23,4 +24,28 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   res.status(200).json(entries);
+};
+
+const createEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description }: { description: string } = req.body;
+
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  });
+
+  try {
+    await db.connect();
+
+    await newEntry.save();
+
+    await db.disconnect();
+
+    return res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+
+    return res.status(500).json({ message: 'Algo salio mal' });
+  }
 };
